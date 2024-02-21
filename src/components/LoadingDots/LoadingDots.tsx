f63ca796057a6de1ee7a3e2172991445fdad4c5f
Omar Yasser <omaryasser_250@hotmail.com>
2024-02-21 18:06:46 +0200

    feat: add LoadingDots to indicate loading

---
+++ src/components/LoadingDots/LoadingDots.tsx
@@ -0,0 +1,17 @@
+import classes from "./LoadingDots.module.css";
+
+function LoadingDots() {
+  return (
+    <div className={classes.loadingDots}>
+      <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+        <g fill="white">
+          <circle cx="10" cy="50" r="10" />
+          <circle cx="40" cy="50" r="10" />
+          <circle cx="70" cy="50" r="10" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+export default LoadingDots;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { MainNav, MainNavButton } from "./MainNav";
 export { default as Logo } from "./Logo";
 export { default as HomeButton } from "./HomeButton";
+export { default as LoadingDots } from "./LoadingDots";
