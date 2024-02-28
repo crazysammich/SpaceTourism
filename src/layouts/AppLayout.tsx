5d6827250521b348a16c6b2f30f38f188f369283
Omar Yasser <omaryasser_250@hotmail.com>
2024-02-28 12:56:58 +0200

    add loading indicator

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,15 +1,20 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import classes from "./AppLayout.module.css";
 import Header from "./Header";
+import { LoadingDots } from "../components";
 
 function AppLayout() {
   const pathname = useLocation().pathname.replace("/", "");
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
     <div
       className={`${classes.pageWrapper} ${
         pathname ? classes[`page-${pathname}`] : ""
       }`}
     >
+      {isLoading && <LoadingDots />}
       <Header />
       <div className="mainWrapper">
         <Outlet />
