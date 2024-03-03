d92d7afc08c0f919d10348f5de0c5550e98944cf
Omar Yasser <omaryasser_250@hotmail.com>
2024-03-03 15:33:16 +0200

    style: change LoadingDots animation

--- src/components/LoadingDots/LoadingDots.tsx
+++ src/components/LoadingDots/LoadingDots.tsx
@@ -4,10 +4,17 @@ function LoadingDots() {
   return (
     <div className={classes.loadingDots}>
       <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
-        <g fill="white">
+        <g fill="grey">
           <circle cx="10" cy="50" r="10" />
           <circle cx="40" cy="50" r="10" />
           <circle cx="70" cy="50" r="10" />
+          <circle
+            cx="70"
+            cy="50"
+            r="10"
+            fill="white"
+            className={classes.mainDot}
+          />
         </g>
       </svg>
     </div>
