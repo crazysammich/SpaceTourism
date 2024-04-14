0f5e7a5ff49d127b7230dc3c6f3a49db6c177d1a
Omar Yasser <omaryasser_250@hotmail.com>
2024-04-14 17:24:58 +0200

    chore: update ravenwaven's components lib

--- src/lib/Carousel.tsx
+++ src/lib/Carousel.tsx
@@ -6,15 +6,25 @@ interface CarouselProps {
   className?: string;
   items: JSX.Element[];
   Indicator?: ComponentType<IndicatorProps>;
+  autoPlay?: { isEnabled?: boolean; delay?: number };
+  onIndictClick?: (index: number) => void;
 }
 
-function Carousel({ className, items, Indicator }: CarouselProps) {
+function Carousel({
+  className,
+  items,
+  Indicator,
+  autoPlay,
+  onIndictClick,
+}: CarouselProps) {
   return (
     <>
       <Cs
         className={className ? className : ""}
         items={items}
         Indicator={Indicator}
+        autoPlay={autoPlay}
+        onIndictClick={onIndictClick}
       />
     </>
   );
