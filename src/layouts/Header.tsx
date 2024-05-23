952afc9a1e0a8f78d3f4bc21bc248330f6cce3c0
Omar Yasser <omaryasser_250@hotmail.com>
2024-05-23 17:53:59 +0300

    feat: toggle header visibility on scrolling up and down

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,11 +1,58 @@
+import { useEffect, useRef, useState } from "react";
 import { Navbar } from "../components";
 import classes from "./Header.module.css";
 function Header() {
+  const headerRef = useRef<HTMLElement>(null);
+  const lastKnownScrollY = useRef<number>(0);
+  const [isHeaderVisible, setIsHeaderVisible] = useState(true);
+
+  useEffect(() => {
+    const onPageScroll = throttle(() => {
+      if (window.scrollY < lastKnownScrollY.current) {
+        setIsHeaderVisible(true);
+        lastKnownScrollY.current = window.scrollY;
+      } else {
+        setIsHeaderVisible(false);
+        lastKnownScrollY.current = window.scrollY;
+      }
+    }, 0.25);
+    document.addEventListener("scroll", onPageScroll);
+    return () => {
+      document.removeEventListener("scroll", onPageScroll);
+    };
+  }, [isHeaderVisible]);
+
   return (
-    <header className={classes.header}>
+    <header
+      className={`${classes.header} ${isHeaderVisible ? classes.visible : ""}`}
+      ref={headerRef}
+    >
       <Navbar />
     </header>
   );
+}
+
+function throttle(cb: () => void, delay = 1) {
+  let isWaiting = false;
+  let pendingArgs: [] | null = null;
+  const timeoutFunc = () => {
+    if (!pendingArgs) {
+      isWaiting = false;
+    } else {
+      cb(...pendingArgs);
+      pendingArgs = null;
+      setTimeout(timeoutFunc, delay * 1000);
+    }
+  };
+  return function (...args: []) {
+    if (isWaiting) {
+      pendingArgs = args;
+      return;
+    }
+    cb(...args);
+    isWaiting = true;
+    setTimeout(timeoutFunc, delay * 1000);
+  };
 }
 
 export default Header;
