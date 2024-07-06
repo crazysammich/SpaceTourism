950ad93e83628fc3c0d8614c2b9306ab5d1c398e
Omar Yasser <omaryasser_250@hotmail.com>
2024-07-06 20:55:34 +0300

    feat: add loading indicator to destination image

--- src/components/CrewImage/CrewImage.tsx
+++ src/components/CrewImage/CrewImage.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { CrewType } from "../../types";
 import classes from "./CrewImage.module.css";
 import { Image } from "@crazysammich/react-components-lib";
+import LoadingDots from "../LoadingDots";
 
 interface CrewItemProps {
   className?: string;
@@ -8,12 +10,19 @@ interface CrewItemProps {
 }
 
 function CrewImage({ className, currentCrew }: CrewItemProps) {
+  const [isImgLoading, setIsImgLoading] = useState(true);
+
+  function handleOnImgLoad() {
+    setIsImgLoading(false);
+  }
   return (
     <div className={`${classes.crewImgWrapper} ${className ? className : ""}`}>
+      {isImgLoading && <LoadingDots />}
       <Image
         className={`fade-in ${classes.crewImg}`}
         src={currentCrew.images.webp}
         alt={`an image of ${currentCrew.role}. ${currentCrew.name}`}
+        onLoad={handleOnImgLoad}
       />
     </div>
   );
