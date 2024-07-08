9de2a458b76dd1d04dc4daa9127d923be0469176
Omar Yasser <omaryasser_250@hotmail.com>
2024-07-08 16:40:03 +0300

    refactor: replace internal useThrottle with third-party useThrottle

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,22 +1,22 @@
 import { useEffect, useRef, useState } from "react";
 import { Navbar } from "../components";
 import classes from "./Header.module.css";
-import { useThrottle } from "../hooks";
+import { useThrottle } from "@crazysammich/react-components-lib";
 
 function Header() {
   const prevScrollPos = useRef<number>(0);
   const [isHeaderVisible, setIsHeaderVisible] = useState(true);
-  const throttle = useThrottle();
-  const onPageScroll = () => {
+  const onPageScrollThrottled = useThrottle(onPageScroll, 0.25);
+
+  function onPageScroll() {
     const currentScrollPos = window.scrollY;
     setIsHeaderVisible(
       currentScrollPos < prevScrollPos.current || currentScrollPos < 20
     );
     prevScrollPos.current = currentScrollPos;
-  };
+  }
 
   useEffect(() => {
-    const onPageScrollThrottled = throttle(onPageScroll, 0.25);
     document.addEventListener("scroll", onPageScrollThrottled);
     return () => {
       document.removeEventListener("scroll", onPageScrollThrottled);
